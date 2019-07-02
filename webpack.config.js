b431ee79d6c71abb3625dbae313cbff755ec433c
dqhan <291927006@qq.com>
2019-07-02 21:22:29 +0800

    webpack初始化

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    mode: "development",
+    entry: './app.js',
+    output: {
+        path: path.resolve(__dirname, './build/'),
+        filename: "source.js",
+    },
+    watchOptions: {
+        aggregateTimeout: 800,
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+
+                    }
+                }
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!less-loader"
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'url-loader',
+                }
+            }
+        ]
+    },
+    plugins: [
+    ],
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".css"]
+    },
+    externals: {
+
+    }
+}
